d400e630b69e9789699f265f2ca565f4bb765654
JoaoMello19 <joaomgvitor@hotmail.com>
2026-09-03 21:37:39 -0300

    Criando a estrutura base

---
+++ devocionario/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+    return <>
+        <header>
+            <h1>Meu Devocionário</h1>
+        </header>
+        <aside></aside>
+        <main id="main">
+
+        </main>
+    </>;
+}
+
+export default App;
