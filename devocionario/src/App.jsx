4dda6c37e5a5014a85102ea0a5cf354838c605c7
JoaoMello <joaomgvitor@hotmail.com>
2026-09-10 16:46:02 -0300

    Iniciando a estrutura lateral

--- devocionario/src/App.jsx
+++ devocionario/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 
 function App() {
-    return <>
-        <header>
-            <h1>Meu Devocionário</h1>
-        </header>
-        <aside></aside>
-        <main id="main">
-
+    return (
+        <main>
+            <aside>
+                <img src="./src/assets/crucifix.png" alt="" />
+                <h1>Meu Devocionário</h1>
+                <h2>Um caminho de fé, todos os dias</h2>
+            </aside>
+            <main id="main"></main>
         </main>
-    </>;
+    );
 }
 
 export default App;
